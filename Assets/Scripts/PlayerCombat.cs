5deca6050fff3f99bffad472ac1c8a674b3538cf
sklobo <sujay.lobo@student.unimelb.edu.au>
2025-03-01 08:39:53 +1100

    Firing implemented with limited lifetime bullets and cooldown

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,13 +7,27 @@ public class PlayerCombat : MonoBehaviour
 
     [SerializeField]
     private GameObject bullet;
-
+    [SerializeField]
+    private Transform projectileContainer;
     [SerializeField]
     private Transform leftTurret;
     [SerializeField]
     private Transform rightTurret;
 
+    [SerializeField]
+    private float bulletVelocity;
+    [SerializeField]
+    private float bulletLifetime;
+
+    [SerializeField,Range(0,20)]
+    private float shootCooldown = 1.2f;
+
+    [SerializeField]
+    private bool canFire;
+
     private Transform bulletSpawn;
+    private float bulletTimer;
+
 
     private InputAction attackAction;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -23,17 +38,34 @@ public class PlayerCombat : MonoBehaviour
 
         bulletSpawn = leftTurret;
         attackAction = InputSystem.actions.FindAction("Attack");
+
+        bulletTimer = 0.0f;
+        canFire = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        bulletTimer += Time.deltaTime;
+        if (bulletTimer >= shootCooldown)
+        {
+            canFire = true;       
+            bulletTimer = 0.0f;
+        }
         bool attackButtonPressed = attackAction.IsPressed();
 
-        if (attackButtonPressed)
+        if (attackButtonPressed && canFire)
         {
-            Instantiate(bullet,bulletSpawn);
-            bulletSpawn = (bulletSpawn == leftTurret) ? rightTurret : leftTurret;
+            FireBullet();
         }
+    }
+
+    void FireBullet()
+    {
+            canFire = false;
+            GameObject bulletInstance = Instantiate(bullet,bulletSpawn.position,bulletSpawn.rotation * Quaternion.Euler(90,0,0),projectileContainer);
+            bulletInstance.GetComponent<Bullet>()._velocity = bulletVelocity;
+            Destroy(bulletInstance,bulletLifetime);
+            bulletSpawn = (bulletSpawn == leftTurret) ? rightTurret : leftTurret;
     }
 }
